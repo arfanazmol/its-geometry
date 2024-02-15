ece8e032c7cf7e88766fe034e588b46e8bb11c58
arfanazmol <arfanazmol66@gmail.com>
2024-02-15 07:13:33 +0600

    Light exercise

--- rectangle-area.js
+++ rectangle-area.js
@@ -1,3 +1,24 @@
-function rectangleArea(){
-    console.log('hate lage betha reeee');
+function calculateRectangleArea(){
+const lengthInput = document.getElementById('rectangle_length');
+const lengthText = lengthInput.value;
+const length = parseFloat(lengthText)
+console.log(length);
+
+
+// get width of the  rectangle
+const widthInput = document.getElementById('rectangle_width');
+const widthText = widthInput.value;
+const width = parseFloat(widthText)
+console.log(widthText);
+
+//calculate recangle area
+const area = length * width;
+console.log('jibone valo vabe kono kisu shikini:',area);
+
+
+// display rectangle area
+const rectangleSpanArea = document.getElementById('rectangle_area');
+rectangleSpanArea.innerText = area;
 }
+
+  
